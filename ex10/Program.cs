8d8c93e15fabd9007d7d9e533d526bac4fe0c8e7
Yury Dudin <cepob33@gmail.com>
2023-05-24 13:18:47 +0500

    Решил задачу 10

---
+++ ex10/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+void FindSecNumb(int number)
+{
+int a = (number / 10) % 10;
+Console.WriteLine(a);
+}
+int b = int.Parse(Console.ReadLine());
+FindSecNumb(b);
+
+
+
